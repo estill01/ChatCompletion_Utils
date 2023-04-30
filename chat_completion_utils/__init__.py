1a14073ab6461d127ae0e9ddb998a6275a61f49a
estill01 <ehs.stillman@gmail.com>
2023-04-30 00:29:28 -0700

    Published to pypi

--- chat_completion_utils/__init__.py
+++ chat_completion_utils/__init__.py
@@ -1,1 +1,1 @@
-from main import *
+from .chat_utils import *
--- chat_completion_utils/chat_utils.py
+++ chat_completion_utils/chat_utils.py
@@ -1,22 +1,51 @@
 import openai
 import os
 import tiktoken
-from typing import List
+from typing import List, Dict
 
-openai.api_key=os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-def llm(system_instruction: str = None, user_input: str = None, temp: int = 0, messages: List[Dict[str, str]] = []) -> str:
+MODELS = {
+    'models': ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-32k'],
+    'gpt-3.5-turbo': {
+        'model': 'gpt-3.5-turbo',
+        'max_tokens': 4096,
+    },
+    'gpt-4': { 
+        'model': 'gpt-4',
+        'max_tokensi': 8192,
+    },
+    'gpt-4-32k': {
+        'modeli': 'gpt-4-32k',
+        'max_tokens': 32768,
+    },
+}
+
+DEFAULT_MODEL=MODELS['gpt-4']['model']
+
+def set_default_model(model_family: str='gpt-4') -> None:
+    DEFAULT_MODEL=model_family
+
+# ---------------------------------------------------------
+
+def llm(
+    system_instruction: str = None, 
+    user_input: str = None, 
+    messages: List[Dict[str, str]] = [], 
+    temp: int = 0, 
+    model_family: str = DEFAULT_MODEL,
+) -> str:
     messages = _build_prompt(
-        system=system_instruction
+        system=system_instruction,
         user=user_input,
         messages=messages,
     )
-    model = select_model(messages)
+    model = select_model(messages, DEFAULT_MODEL)
     response = openai.ChatCompletion.create(
-        model=model
+        model=model,
         messages=messages,
         temperature=temp,
-        max_tokens=MODELS[model].max_tokens
+        max_tokens=MODELS[model].max_tokens,
     )
     return response.choices[0].message.content
 
@@ -67,7 +96,7 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
     num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
     return num_tokens
 
-def select_model(messages: List[Dict[str, str]], model_family: str = 'gpt-4', force: bool = False) -> str:
+def select_model(messages: List[Dict[str, str]], model_family: str = DEFAULT_MODEL, force: bool = False) -> str:
     msg_count = len(messages)
     token_count = num_tokens_from_messages(messages, model_family)
     avg_tokens_message = int(token_count / msg_count)
@@ -102,26 +131,6 @@ def build_prompt(
         return messages.extend([_make_system_prompt(system_content), _make_user_prompt(user_content)])
     if system_content is None and user_content is not None:
         return messages.extend([_make_user_prompt(user_content)])
-    else
+    else:
         raise ValueError("Must provide `user_content`")
 
-
-# ----------------------------------------------------
-# CONSTANTS
-# ----------------------------------------------------
-
-MODELS = {
-    'models': ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-32k']
-    'gpt-3.5-turbo': {
-        model: 'gpt-3.5-turbo',
-        max_tokens: 4096,
-    },
-    'gpt-4': { 
-        model: 'gpt-4',
-        max_tokens: 8192,
-    },
-    'gpt-4-32k': {
-        model: 'gpt-4-32k',
-        max_tokens: 32768,
-    }
-}
